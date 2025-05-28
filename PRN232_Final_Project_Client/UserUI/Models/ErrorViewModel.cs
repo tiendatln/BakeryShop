516d6b99aa1c750664a231e5c6267d39c1516c9a
NhanPham <phamhiennhan3105@gmail.com>
2025-05-28 15:45:51 +0700

    Initial commit

---
+++ PRN232_Final_Project_Client/UserUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UserUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
